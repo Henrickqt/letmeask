fce918b7c6a66d1d25dab7702116120a077a640b
Henrickqt <pedro.msg@hotmail.com>
2021-07-05 19:48:51 -0300

    Creating Question

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,6 +6,7 @@ import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 
 import logoImg from '../assets/images/logo.svg';
 
@@ -21,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isAnswered: boolean;
 }>
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -40,7 +41,7 @@ export function Room() {
   const params = useParams<RoomParams>();
   const { user } = useAuth();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
   const roomId = params.id;
@@ -126,6 +127,14 @@ export function Room() {
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
+
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question key={question.id} content={question.content} author={question.author} />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
